1d62d12fdb6f06aec433180654f18399f9cb7b80
Taras Markov <10aquila07@gmail.com>
2024-04-23 22:12:59 +0300

    token

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const API_URL = 'https://api.github.com/repos/TarasMarkov6/training/contents/fighters.json';
+const token = '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';
+
+const SECURITY_HEADERS = {
+  headers: {
+    Authorization: `token ${token}`
+  }
+};
 
 const promise = fetch(API_URL, SECURITY_HEADERS);
 console.log(promise);
