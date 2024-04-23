87721fcfc49446cffed022a2ef39be2d137aa382
Taras Markov <10aquila07@gmail.com>
2024-04-23 22:04:04 +0300

    next try

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const API_URL = 'https://api.github.com/repos/TarasMarkov6/training/contents/fighters.json';
-const token = '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';
+//const API_URL = 'https://api.github.com/repos/TarasMarkov6/training/contents/fighters.json';
+//const token = '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';
 
 const SECURITY_HEADERS = {
   headers: {
