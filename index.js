c8900b0a286b88bbf584787285e4d48d9d67a1f6
Taras Markov <10aquila07@gmail.com>
2024-04-24 21:58:00 +0300

    fetch method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://api.github.com/repos/TarasMarkov6/training/contents/fighters.json';
-const token = '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';
+const token = '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';
 
 const SECURITY_HEADERS = {
   headers: {
@@ -7,13 +7,28 @@ const SECURITY_HEADERS = {
   }
 };
 
-const promise = fetch(API_URL, SECURITY_HEADERS);
-console.log(promise);
-promise.then(response => {
-    const a = response.json();
-    console.log(a);
-    return a;
-})
-.then(json=>{
-    console.log(JSON.parse(atob(json.content)))
-})
+const rootElement = document.getElementById('root');
+rootElement.innerText = 'Loading...';
+
+fetch(API_URL, SECURITY_HEADERS)
+  .then(response => {
+    if (!response.ok) {
+      //throw new Error ('Failed to load data');
+      return Promise.reject(new Error ('Failed to load data'));
+    }
+
+    return response.json();
+  })
+  .catch(error => {
+    console.warn(error);
+    rootElement.innerText = 'Failed to load data';
+  });
+  /*
+  .then(file => {
+    const fighters = JSON.parse(atob(file.content));
+    const names = fighters.map(item=>item.name);
+    const namesStr = names.join('\n');
+    rootElement.innerText = namesStr;
+    console.log(namesStr);
+  });
+  */
